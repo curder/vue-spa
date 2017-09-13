0eefe369bb05e6a6c94bdf63c7055d4f3b2d38f6
curder <curder@foxmail.com>
2017-09-13 13:44:12 +0800

    vee-validate表单验证

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,8 +5,10 @@ window.Vue = require('vue');
 import VueRouter from "vue-router";
 import router from "./routers";
 import App from "./components/App";
+import VeeValidate from "vee-validate";
 
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
 Vue.component('App', App);
 
 const app = new Vue({
